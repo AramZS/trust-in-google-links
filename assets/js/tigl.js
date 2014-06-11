9ea1d097d0554e7090c26f5ee47bba26cb8c5763
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-06-11 19:14:25 -0400

    It lives!
    AJAX FTW!

--- assets/js/tigl.js
+++ assets/js/tigl.js
@@ -1,25 +1,37 @@
 jQuery(window).load(function() {
-    jQuery('#poststuff').on('click', '.mce-i-link', function(evt){
+    jQuery('#poststuff').on('mousedown', '.mce-i-link', function(evt){
         var term = "";
         if (window.getSelection){
-            term = window.getSelection().toString();
+            var iframe = document.getElementById('content_ifr');
+            var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+            var innerSelection = innerDoc.getSelection();
+            term = innerSelection.toString();
         } else if (document.selection && document.selection.type != "Control") {
             term = document.selection.createRange().text;   
         }
         var safeTerm = encodeURIComponent(term); 
         var googleLucky = 'http://www.google.com/search?h1=en&q='+safeTerm+'&btnI=1';
+        jQuery('#wp-link-backdrop').prepend('<h1 id="keymaster" style="color:white;font-weight: bolder;width: 580px;height: 100px;margin: 7px auto;">Standby for Autolink. Are you the gatekeeper?</h1>');
+        jQuery.post(ajaxurl, {
+			action: 'get_lucky_with_google',
+			google_lucky_url: googleLucky
+		},
+		function(response) {
+			var firstLink = jQuery(response).find("response_data").text();
+            jQuery('#link-options #url-field').val(firstLink);
+            jQuery('#link-options #link-title-field').val(term);
+            jQuery('#keymaster').remove();
+            jQuery('#link-target-checkbox').prop('checked', true);
+			//jQuery(".nominate-result-"+itemID).html(response);
+			//alert(response);
+			//jQuery("#test-div1").append(data);
+		});          
         
-        var req = new XMLHttpRequest();
-        req.open('GET', googleLucky, false);
-        req.send(null);
-        # Replace this with AJAX        
-        var firstLink = req.getResponseHeader('Location');
         
-        jQuery('#link-options #url-field').val(firstLink);
         
     });
 
-    jQuery('#poststuff').on('click', '.mce_link', function(evt){
+    jQuery('#poststuff').on('mousedown', '.mce_link', function(evt){
         var term = "";
         if (window.getSelection){
             term = window.getSelection().toString();
@@ -28,14 +40,18 @@ jQuery(window).load(function() {
         }
         var safeTerm = encodeURIComponent(term); 
         var googleLucky = 'http://www.google.com/search?h1=en&q='+safeTerm+'&btnI=1';
+        jQuery.post(ajaxurl, {
+			action: 'get_lucky_with_google',
+			google_lucky_url: googleLucky
+		},
+		function(response) {
+			var firstLink = jQuery(response).find("response_data").text();
+            jQuery('#link-options #url-field').val(firstLink);
+			//jQuery(".nominate-result-"+itemID).html(response);
+			//alert(response);
+			//jQuery("#test-div1").append(data);
+		});          
         
-        var req = new XMLHttpRequest();
-        req.open('GET', googleLucky, false);
-        req.send(null);
-                
-        var firstLink = req.getResponseHeader('Location');
-        
-        jQuery('#link-options #url-field').val(firstLink);
         
     });    
     
--- trust-in-google-links.php
+++ trust-in-google-links.php
@@ -15,3 +15,33 @@ function trust_in_google_by_adding_admin_scripts(){
 }
 
 add_action( 'admin_enqueue_scripts', 'trust_in_google_by_adding_admin_scripts' );
+
+function get_lucky_with_google(){
+    ob_start();
+    $url = $_POST['google_lucky_url'];
+
+    #http://motyar.blogspot.com/2011/08/get-first-link-in-google-search-for.html
+    
+    //Get the headers
+    $headers = get_headers($url, 1);
+    //The url for redirect
+    $url = $headers['Location'];
+
+			$response = array(
+				'what' => 'link',
+				'action' => 'get_lucky_with_google',
+				'id' => 1,
+				'data' => $url,
+				'supplemental' => array(
+					'origin' => $url,
+					'buffered' => ob_get_contents()
+				)
+			);
+			$xmlResponse = new WP_Ajax_Response($response);
+			$xmlResponse->send();
+		ob_end_flush();
+    
+}
+
+
+add_action( 'wp_ajax_get_lucky_with_google', 'get_lucky_with_google' );
